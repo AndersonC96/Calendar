6de988b38a4a3b9fd2d0e0905cf65a85887b8df1
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-05-19 16:40:36 -0300

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,8 +1,8 @@
 <?php
     try{
-        $dbd=new PDO('mysql:host=localhost;dbname=calendar;charset=utf8', 'root', '');
+	    $bdd = new PDO('mysql:host=localhost;dbname=calendar;charset=utf8', 'root', '');
     }
     catch(Exception $e){
-        die('Error :'.$e->getMensage());
-    }
+        die('Error : '.$e->getMessage());
+}
 ?>
